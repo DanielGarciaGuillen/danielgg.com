9232b5d747848f25c7d911869a74a5281f60d045
DanielGarciGuillen <danielgarciaguillen22@gmail.com>
2019-04-11 22:42:50 +0200

    small fix

--- src/components/index/Portfolio.js
+++ src/components/index/Portfolio.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
-
 import dedd from '../../images/dedd/dedd.png'
 import ott from '../../images/ottawatech/ott.png'
 import reactimage from '../../images/icons/react.png'
@@ -72,20 +71,17 @@ const IconDiv = styled.div`
 display: flex;
 justify-content:space-evenly;
 padding-bottom: 80px
-
 }
 `
 
-const ProjectImage = styled.img`
-  
+const ProjectImage = styled.img`  
   width: 700px;
   transition: all .2s ease-in-out;
   &:hover {
     transform: scale(1.05);
     border: 10px solid transparent; 
     border-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='112' height='92' viewBox='0 0 112 92'%3E%3Cg fill='%236bd2ed' fill-opacity='0.36'%3E%3Cpath fill-rule='evenodd' d='M72 10H40L16 20H0v8h16l24-14h32l24 14h16v-8H96L72 10zm0-8H40L16 4H0v8h16l24-6h32l24 6h16V4H96L72 2zm0 84H40l-24-6H0v8h16l24 2h32l24-2h16v-8H96l-24 6zm0-8H40L16 64H0v8h16l24 10h32l24-10h16v-8H96L72 78zm0-12H40L16 56H0v4h16l24 14h32l24-14h16v-4H96L72 66zm0-16H40l-24-2H0v4h16l24 6h32l24-6h16v-4H96l-24 2zm0-16H40l-24 6H0v4h16l24-2h32l24 2h16v-4H96l-24-6zm0-16H40L16 32H0v4h16l24-10h32l24 10h16v-4H96L72 18z'/%3E%3C/g%3E%3C/svg%3E")  20% round;
-  }
-  }
+  }  
   @media (max-width: 900px) {
     width: 500px;
   }
@@ -150,7 +146,7 @@ class Portfolio extends Component {
 
           <div>
             <a href="http://ottawatech.io/">
-              <ProjectImage src={ott} />
+              <ProjectImage alt="OttawaTechPhoto" src={ott} />
             </a>
           </div>
 
